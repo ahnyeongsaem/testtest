63c2bd4766871fc6660d39ec15056552cb025250
ahnyeongsaem <ahnsaem90@naver.com>
2015-07-07 15:16:17 +0900

    gongu
    a12345667

--- githubtest/githubtest/Source.cpp
+++ githubtest/githubtest/Source.cpp
@@ -1,8 +1,67 @@
-﻿#include<stdio.h>
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<sstream>
+#include<deque>
+#include<iomanip>
+using namespace std;
 
 
 int main()
 {
 	printf("asdfasdewrtewrewrwsfasd");
 	return 0;
+	int a;
+	cout << a;
+	if (a == 1)
+	{
+		
+
+
+	}
+	else if (a == 2)
+	{
+
+	}
+	else if (a == 3)
+	{
+
+	}
+	else if (a == 4)
+	{
+
+	}
+	else if (a == 5)
+	{
+
+	}
+	else if (a == 6)
+	{
+
+	}
+	else if (a == 7)
+	{
+
+	}
+	else if (a == 8)
+	{
+
+	}
+	else if (a == 9)
+	{
+
+	}
+	else if (a == 10)
+	{
+
+	}
+	else if (a == 11)
+	{
+
+	}
+
+
+
+
 }
