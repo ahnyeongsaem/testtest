eb8b11e0db42c315bdf011f4b35811eed1d3173b
unknown <Heemdoorow>
2015-07-07 15:38:42 +0900

    Revert "Throw Garbage"
    This reverts commit 7142789555219834ef023c7d46bcfa185dea32e6.

---
+++ githubtest/githubtest/Source.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<sstream>
+#include<deque>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+
+int main()
+{
+	printf("asdfasdewrtewrewrwsfasd");
+	return 0;
+	int a;
+	cout << a;
+	if (a == 1)
+	{
+		
+
+
+	}
+	else if (a == 2)
+	{
+
+	}
+	else if (a == 3)
+	{
+
+	}
+	else if (a == 4)
+	{
+
+	}
+	else if (a == 5)
+	{
+
+	}
+	else if (a == 6)
+	{
+
+	}
+	else if (a == 7)
+	{
+
+	}
+	else if (a == 8)
+	{
+
+	}
+	else if (a == 9)
+	{
+
+	}
+	else if (a == 10)
+	{
+
+	}
+	else if (a == 11)
+	{
+
+	}
+
+
+
+
+}
